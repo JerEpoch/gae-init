1598d48fe3898560059a9dfea8cf7569ad229c06
Jeremy <jeremy.studzinski@gmail.com>
2017-09-20 01:21:42 -0500

    more us only shows work

--- main/tvshows.py
+++ main/tvshows.py
@@ -59,9 +59,10 @@ def fake_data(id):
 
 def getAirsToday(page):
 	page = str(page)
+	
 	# gets memcache of shows airing today on the current user page
 	data = memcache.get('dailyTV' + page)
-	
+
 	
 	url = "https://api.themoviedb.org/3/tv/airing_today?page=" + page + "&language=en-US&api_key=" + TMDB_API_KEY
 
@@ -76,15 +77,73 @@ def getAirsToday(page):
 			# just a check to ensure there is data, then adds it to the memcache and returns it.
 			if len(data['results']) > 0:
 				memcache.add('dailyTV' + page, data, time=CACHE_TIME)
+				memcache.add('dailyTV', data, time=CACHE_TIME)
 				return data['results'], data['total_pages']
 			else:
 				return None
 
+def all_shows_daily():
+	# gets all the shows for the day, puts them in a list and then sends them to be sorted by US country and returns that
+	totalShows = []
+	shows = []
+	page = '1'
+	url = "https://api.themoviedb.org/3/tv/airing_today?page=" + page + "&language=en-US&api_key=" + TMDB_API_KEY
+	data = memcache.get('dailyUS')
 
-def sort_list(shows, totalPages, showTime):
+	if data is not None:
+		return data
+	else:
+		result = urlfetch.fetch(url)
+		if result.status_code == 200:
+			data = json.loads(result.content)
+			if data['total_pages'] > 1:
+				for page in range(1, data['total_pages'] + 1):
+					page = str(page)
+					url = "https://api.themoviedb.org/3/tv/airing_today?page=" + page + "&language=en-US&api_key=" + TMDB_API_KEY
+					result = urlfetch.fetch(url)
+					data = json.loads(result.content)
+					shows = data['results']
+					for show in shows:
+						totalShows.append(show)
+
+		sort_shows = sort_list(totalShows)
+		memcache.add('dailyUS', sort_shows, time=CACHE_TIME)
+
+	return sort_shows
+
+def all_shows_weekly():
+	totalShows = []
+	shows = []
+	page = '1'
+	url = "https://api.themoviedb.org/3/tv/on_the_air?page=" + page +"&language=en-US&api_key=" + TMDB_API_KEY
+	data = memcache.get('weeklyUS')
+
+	if data is not None:
+		return data
+	else:
+		result = urlfetch.fetch(url)
+		if result.status_code == 200:
+			data = json.loads(result.content)
+			if data['total_pages'] > 1:
+				for page in range(1, data['total_pages'] + 1):
+					page = str(page)
+					url = "https://api.themoviedb.org/3/tv/on_the_air?page=" + page +"&language=en-US&api_key=" + TMDB_API_KEY
+					result = urlfetch.fetch(url)
+					data = json.loads(result.content)
+					shows = data['results']
+					for show in shows:
+						totalShows.append(show)
+
+		sort_shows = sort_list(totalShows)
+		memcache.add('weeklyUS', sort_shows, time=CACHE_TIME)
+
+	return sort_shows
+
+def sort_list(shows):
 	# returns a list with US only shows.
 	# showTime for either daily or weekly shows
 	new_data = []
+
 	
 	for show in shows:
 		if show['origin_country']:
@@ -134,18 +193,19 @@ def getSingleShowInfo(id):
 				logging.exception('Caught exception fetching url')
 
 # used for testing stuff
-def getShowTest(page):
-
-	page = str(page)
-	url = "https://api.themoviedb.org/3/tv/airing_today?page=" + page + "&language=en-US&api_key=" + TMDB_API_KEY
-	url2 = "https://api.themoviedb.org/3/tv/airing_today?api_key=3a3628871c75cfc1fa3bcf7b2f9043aa&language=en-US&page=2"
-	result = urlfetch.fetch(url)
-	result2 = urlfetch.fetch(url2)
-	if result.status_code == 200:
-		data = json.loads(result.content)
-		data2 = json.loads(result2.content)
-		newData = dict(data.items() + data2.items())
-		return newData['results'], data['total_pages']
+# def getShowTest(page):
+# 	totalShows = []
+# 	getAirsToday(1, totalShows)
+# 	page = str(page)
+# 	url = "https://api.themoviedb.org/3/tv/airing_today?page=" + page + "&language=en-US&api_key=" + TMDB_API_KEY
+# 	url2 = "https://api.themoviedb.org/3/tv/airing_today?api_key=3a3628871c75cfc1fa3bcf7b2f9043aa&language=en-US&page=2"
+# 	result = urlfetch.fetch(url)
+# 	result2 = urlfetch.fetch(url2)
+# 	if result.status_code == 200:
+# 		data = json.loads(result.content)
+# 		data2 = json.loads(result2.content)
+# 		newData = dict(data.items() + data2.items())
+# 		return newData['results'], data['total_pages']
 
 
 def getShowDetails(data):
@@ -322,11 +382,12 @@ def remove_favorite(id):
 		flask.flash("Removed from favorites.", category='success')
 	return flask.redirect(flask.url_for('my_favorites'))
 
-@app.route('/daily/<int:page>/', methods=['GET', 'POST'])
-def shows_today(page):
-	shows, totalPages = getAirsToday(page)
+#@app.route('/daily/<int:page>/', methods=['GET', 'POST'])
+@app.route('/daily/', methods=['GET', 'POST'])
+def shows_today():
+	#shows, totalPages = getAirsToday(page)
 	#flask.flash(shows, category='success')
-
+	shows = all_shows_daily()
 	#sortShows = sort_list(shows)
 	#flask.flash(sortShows)
 	if shows:
@@ -335,25 +396,22 @@ def shows_today(page):
 																html_class = 'todays-shows',
 																shows = shows,
 																head = head,
-																page = page,
-																totalPages = totalPages,
 																back_url = 'shows_today'
 																)
 	else:
 		errors = "Something went wrong fetching the current day's shows. Please try again later."
 		return flask.render_template('search_error.html', html_class='search-error', errors=errors)
 
-@app.route('/weekly/<int:page>/', methods=['GET', 'POST'])
-def shows_weekly(page):
-	shows, totalPages = getAirsWeek(page)
+@app.route('/weekly/', methods=['GET', 'POST'])
+def shows_weekly():
+	#shows, totalPages = getAirsWeek(page)
+	shows = all_shows_weekly()
 	if shows:
 		head = "Weekly Shows"
 		return flask.render_template('tvShows.html',
 																html_class = 'todays-shows',
 																shows = shows,
 																head = head,
-																page = page,
-																totalPages = totalPages,
 																back_url = 'shows_weekly'
 																)
 	else:
@@ -364,17 +422,19 @@ def shows_weekly(page):
 # used for testing purposes
 @app.route('/shows/test/', methods=['GET','POST'])
 def show_info():
+	shows = all_shows_weekly()
+	#shows, total = getAirsToday(1)
 	#shows, totalPages = getShowTest(page)
-	shows, totalPages = getAirsToday(1)
-	showsTwo, totalPages = getAirsToday(2)
-	USshows = sort_list(shows, totalPages, daily)
-	USshows_two = sort_list(showsTwo, totalPages, daily)
-	for show in USshows_two:
-		USshows.append(show)
+	# shows, totalPages = getAirsToday(1)
+	# showsTwo, totalPages = getAirsToday(2)
+	# USshows = sort_list(shows, totalPages, 'daily')
+	# USshows_two = sort_list(showsTwo, totalPages, 'daily')
+	# for show in USshows_two:
+	# 	USshows.append(show)
 	#USshows.append(USshows_two)
-	flask.flash(totalPages)
+	#flask.flash(totalPages)
 	#shows = [{u'origin_country': [u'US'], u'poster_path': u'/kwThj8DRX0FB9ACIhggDUoUYcSt.jpg', u'name': u'Big Brother', u'overview': u'Big Brother is a television reality game show based on an originally Dutch TV series of the same name created by producer John de Mol in 1997. The show follows a group of HouseGuests living together 24 hours a day in the "Big Brother" house, isolated from the outside world but under constant surveillance with no privacy for three months. Since its television debut in 2000, Big Brother has run continuously with at least one season of the show airing each year. It is currently the second longest running version in the world to have done so, after the Spanish version. The HouseGuests compete for the chance to win a $500,000 grand prize by avoiding weekly eviction, until the last HouseGuest remains at the end of the season that can claim the $500,000 grand prize. The American series is hosted by television personality Julie Chen. Produced by Allison Grodner and Rich Meehan for Fly On The Wall Entertainment, it currently airs in the United States on CBS and Global.\n\nThe show\'s debut season followed the format of most international editions of the series, in which a group of contestants live together and are voted off each week by the viewers. Following a negative critical and commercial reaction to the first season, the format for future changes was drastically changed. For this new format, a group of contestants, known as "HouseGuests," compete to win the series by voting each other off and being the last HouseGuest remaining. One HouseGuest, known as the Head of Household, must nominate two of their fellow HouseGuests for eviction. The winner of the Power of Veto can remove one of the nominees from the block, forcing the HoH to nominate another HouseGuest. The HouseGuests then vote to evict one of the nominees, and the HouseGuest with the most votes is evicted. When only two HouseGuests remained, the last seven evicted HouseGuests, known as the Jury of Seven, would decide which of them would win the $500,000 prize. Much like the first season, the HouseGuests are still under constant surveillance and are filmed at all times. The September 5, 2013 episode marked the show\'s 500th episode.', u'popularity': 72.880838, u'original_name': u'Big Brother', u'id': 10160, u'vote_average': 4.1, u'vote_count': 80, u'first_air_date': u'2000-07-05', u'original_language': u'en', u'backdrop_path': u'/v5ltIEtD9vvfYCFlfMGv1arTWr3.jpg', u'genre_ids': [10764]}, {u'origin_country': [u'US'], u'poster_path': u'/6Fp49zU7FjsJ4xvvZjCZBiBq9KQ.jpg', u'name': u'Zoo', u'overview': u'Set amidst a wave of violent animal attacks sweeping across the planet, a young renegade scientist is thrust into a race to unlock the mystery behind this pandemic before time runs out for animals and humans alike.', u'popularity': 62.38694, u'original_name': u'Zoo', u'id': 62517, u'vote_average': 5.9, u'vote_count': 124, u'first_air_date': u'2015-06-30', u'original_language': u'en', u'backdrop_path': u'/iiCCD2IEDDNSRSmWYHxw6epMNw5.jpg', u'genre_ids': [18, 10759, 10765]}, {u'origin_country': [u'US'], u'poster_path': u'/tu1UNK5AubGdGco3B2Qy8BjFMvU.jpg', u'name': u'General Hospital', u'overview': u'Families, friends, enemies and lovers experience life-changing events in the large upstate New York city of Port Charles, which has a busy hospital, upscale hotel, cozy diner and dangerous waterfront frequented by the criminal underworld.', u'popularity': 57.376718, u'original_name': u'General Hospital', u'id': 987, u'vote_average': 4.6, u'vote_count': 90, u'first_air_date': u'1963-04-01', u'original_language': u'en', u'backdrop_path': u'/vIDHmF9U0gvQ1Oml9lV1LafHwqb.jpg', u'genre_ids': [80, 18, 10766]}, {u'origin_country': [u'US'], u'poster_path': u'/nouJy2Ba1LMbrTkN1bppnK3G9Do.jpg', u'name': u'The Young and the Restless', u'overview': u'The rivalries, romances, hopes and fears of the residents of the fictional Midwestern metropolis, Genoa City. The lives and loves of a wide variety of characters mingle through the generations, dominated by the Newman, Abbott, Baldwin and Winters families.', u'popularity': 50.651021, u'original_name': u'The Young and the Restless', u'id': 1054, u'vote_average': 5.6, u'vote_count': 19, u'first_air_date': u'1973-08-10', u'original_language': u'en', u'backdrop_path': u'/fMwPsGNZ1zQS20qMjmjRpJx3YqM.jpg', u'genre_ids': [10766]}, {u'origin_country': [u'US'], u'poster_path': u'/7r8AepolaDkK8G6LyzyivkaOA60.jpg', u'name': u'Mountain Men', u'overview': u'Most people enjoy the modern technologies and conveniences of today -- smartphones, tablets, cable and satellite TV among them -- but there are people who choose to live off the grid and in the unspoiled wilderness, where dangers like mudslides, falling trees and bears are all parts of life. "Mountain Men" profiles three such people. Eustace Conway, who has lived at the western edge of the Blue Ridge Mountains in North Carolina for more than 25 years, teaches interns about the old ways of living with nature. Tom Oar needs an entire year to prepare for the seven-month-long winter on Montana\'s Yaak River. In Alaska, Marty Meierotto must gather enough wood to survive, in complete isolation, winters that can have temperatures drop to as low as 60 degrees below zero. It\'s not an easy life but for these mountain men, it\'s life as they know it.', u'popularity': 40.417689, u'original_name': u'Mountain Men', u'id': 60381, u'vote_average': 6.2, u'vote_count': 6, u'first_air_date': u'2012-05-31', u'original_language': u'en', u'backdrop_path': u'/iirIMo6HY1cJnbjskABvtvsz0YG.jpg', u'genre_ids': [10764]}, {u'origin_country': [u'US', u'CA'], u'poster_path': u'/ygFkmE24eUb12zWQNiWb9efjBj9.jpg', u'name': u'Ice Road Truckers', u'overview': u'Take a trip to Yellowknife, Canada to experience one of the most dangerous careers around. In unfathomably cold conditions, truck drivers haul equipment and supplies to miners in the Canadian tundra in the dead of winter on a 350-mile highway of ice.', u'popularity': 37.207304, u'original_name': u'Ice Road Truckers', u'id': 3780, u'vote_average': 4.7, u'vote_count': 12, u'first_air_date': u'2007-06-17', u'original_language': u'en', u'backdrop_path': u'/uRbRavVTO1FqwMSnJjTkS0kn1E5.jpg', u'genre_ids': [10764]}, {u'origin_country': [u'US'], u'poster_path': u'/aQMN0xwSzRanStmfyz7qHP4U7LO.jpg', u'name': u'The Guest Book', u'overview': u'The stories of the vacation home Froggy Cottage and its visitors. While the house and cast of characters living in this small mountain town of Mount Trace remain the same, each episode features a different set of vacationing guests.', u'popularity': 37.153925, u'original_name': u'The Guest Book', u'id': 72544, u'vote_average': 7, u'vote_count': 3, u'first_air_date': u'2017-08-03', u'original_language': u'en', u'backdrop_path': u'/4mM13Lh9lifM1lCz8zsf2QSkKe5.jpg', u'genre_ids': [35]}, {u'origin_country': [u'US'], u'poster_path': u'/2PteThhfkwDipsCavIoYz2ka7hE.jpg', u'name': u'Naked SNCTM', u'overview': u'SNCTM is the most exclusive, high-end erotic club ever. Its wealthy members enjoy black tie masquerades, private dinners, and erotic theater. Get to know SNCTM, its creator and its employees in this eye-opening documentary series.', u'popularity': 36.939909, u'original_name': u'Naked SNCTM', u'id': 73473, u'vote_average': 4, u'vote_count': 1, u'first_air_date': u'2017-08-17', u'original_language': u'en', u'backdrop_path': u'/gu3yLUdf7Mg3DMdAgOspUv36BkU.jpg', u'genre_ids': [99]}]
-	return flask.render_template('testing.html', html_class='blah', shows=USshows)
+	return flask.render_template('testing.html', html_class='blah', shows=shows)
 
 
 
